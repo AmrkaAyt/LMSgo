5699957bc8dabf6f0c4fe812ee7f58e2ec8146b2
AmrkaAyt <142752035+AmrkaAyt@users.noreply.github.com>
2023-09-13 20:46:24 +0600

    Create sort.go

---
+++ sort.go
@@ -0,0 +1,30 @@
+func heapSort(arr []int) {
+	n := len(arr)
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(arr, i, n)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, 0, i)
+	}
+}
+
+func heapify(arr []int, i, n int) {
+	largest := i
+	left := 2*i + 1
+	right := 2*i + 2
+
+	if left < n && arr[left] > arr[largest] {
+		largest = left
+	}
+
+	if right < n && arr[right] > arr[largest] {
+		largest = right
+	}
+
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapify(arr, largest, n)
+	}
+}
