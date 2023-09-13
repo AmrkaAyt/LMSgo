bde6f0c882036fa2e6255a6dd7683ed242fed19f
AmrkaAyt <142752035+AmrkaAyt@users.noreply.github.com>
2023-09-13 20:20:25 +0600

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -1,10 +1,24 @@
+// easiest sol
+// func twoSum(nums []int, target int) []int {
+//	for i, r := range nums {
+//		for j := i + 1; j < len(nums); j++ {
+//			if r+nums[j] == target && i < j {
+//				return []int{i, j}
+//			}
+//		}
+//	}
+//	return []int{}
+//}
+
+// best sol
 func twoSum(nums []int, target int) []int {
-	for i, r := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if r+nums[j] == target && i < j {
-				return []int{i, j}
-			}
-		}
-	}
-	return []int{}
+  m := make(map[int]int)
+  for i, n := range nums {
+    j, r := m[target-n]
+    if r {
+      return []int{i, j}
+    }
+    m[n] = i
+  }
+  return []int{}
 }
